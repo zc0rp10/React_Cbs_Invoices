5f663df155e26aecb6dd84567b820ea5a97853ad
zc0rp10 <zcorpio@gmail.com>
2020-04-08 17:48:26 +0200

    Added sort indication icons to TableHeader Comp.

--- src/components/common/TableHeader.js
+++ src/components/common/TableHeader.js
@@ -13,6 +13,36 @@ const TableHeader = ({ columns, columnSort, onSort }) => {
     }
     onSort(newColumnSort);
   }
+
+  function renderSortIcon(column) {
+    if (column.path !== columnSort.path) return null;
+    if (columnSort.order === "asc")
+      return (
+        <svg
+          class="inline-block fill-current text-blue-500"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+        >
+          <path d="M0 0h24v24H0V0z" fill="none" />
+          <path d="M7 14l5-5 5 5H7z" />
+        </svg>
+      );
+    return (
+      <svg
+        class="inline-block fill-current text-blue-500"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+      >
+        <path d="M0 0h24v24H0V0z" fill="none" />
+        <path d="M7 10l5 5 5-5H7z" />
+      </svg>
+    );
+  }
+
   return (
     <thead>
       <tr>
@@ -20,9 +50,9 @@ const TableHeader = ({ columns, columnSort, onSort }) => {
           <th
             key={column.path || column.key}
             onClick={() => raiseSort(column.path)}
-            className="px-4 py-4 border-b text-left"
+            className="px-4 py-4 border-b text-left cursor-pointer"
           >
-            {column.label}
+            {column.label} {renderSortIcon(column)}
           </th>
         ))}
       </tr>
