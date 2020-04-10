9f1ef83de367bcde51702ad21f08d82ce353e232
zc0rp10 <zcorpio@gmail.com>
2020-04-10 13:37:47 +0200

    InvoiceForm now able to C & U invoices.

--- src/components/InvoiceForm.js
+++ src/components/InvoiceForm.js
@@ -1,49 +1,81 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
 
 //Components
-import TextInput from "./common/TextInput";
+import InputText from "./common/InputText";
+import InputSelect from "./common/InputSelect";
+
+//Temp "Database"
+import { getInvoice, saveInvoice } from "../services/tempInvoiceService";
+import { getClients } from "../services/tempClientService";
 
 const InvoiceForm = ({ match, history }) => {
+  const [invoice, setInvoice] = useState({
+    _id: "",
+    invNbr: "",
+    clientId: "",
+    totalAmount: "",
+    date: "",
+  });
+  const [clients, setClients] = useState([]);
+
+  useEffect(() => {
+    setClients(getClients);
+    const invoiceId = match.params.id;
+    //Case 1: New Invoice -> Escape befor checking for match
+    if (invoiceId === "new") return;
+    //Case 2: Incorrect URL/ID i.e not "new" & invoice does not exist. -> Escape if no match is found in DB
+    const inv = getInvoice(invoiceId);
+    if (!inv) return history.replace("/not-found");
+    //Case 3: Invoice does exist - load the invoice to state and populate form
+    setInvoice(mapToViewModel(inv));
+  }, [history, match.params.id]);
+
+  function mapToViewModel(invoice) {
+    return {
+      _id: invoice._id,
+      invNbr: invoice.invNbr,
+      clientId: invoice.clientId,
+      totalAmount: invoice.totalAmount,
+      date: invoice.date,
+    };
+  }
+
   return (
-    <div>
-      <h1 className="text-2xl pb-8">Invoice Form - {match.params.id}</h1>
+    <div className="bg-white shadow-md rounded-md p-8 max-w-screen-xl mx-auto">
+      <h1 className="text-2xl pb-8">Invoice Form</h1>
       <Formik
+        enableReinitialize
         initialValues={{
-          invNbr: "",
-          clientId: "",
-          clientName: "",
-          totalAmount: "",
-          status: "",
-          date: "",
+          invNbr: invoice.invNbr,
+          clientId: invoice.clientId,
+          totalAmount: invoice.totalAmount,
+          date: invoice.date || new Date().getTime(),
         }}
         validationSchema={Yup.object({
           invNbr: Yup.number().required("Required"),
           clientId: Yup.string().required("Required"),
-          clientName: Yup.string().required("Required"),
           totalAmount: Yup.number().required("Required"),
-          status: Yup.string().required("Required"),
-          date: Yup.date().required("Required"),
+          date: Yup.number().required("Required"),
         })}
         onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            console.table(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 200);
+          saveInvoice(values);
+          history.push("/invoices");
+          setSubmitting(false);
         }}
       >
-        <Form>
-          <TextInput label="Invoice #" name="invNbr" type="number" />
-          <TextInput label="Client Id" name="clientId" type="string" />
-          <TextInput label="Client Name" name="clientName" type="string" />
-          <TextInput label="Total Amount" name="totalAmount" type="number" />
-          <TextInput label="Payment Status" name="status" type="string" />
-          <TextInput label="Invoice Date" name="date" type="date" />
-          <button className="btn btn-primary mt-5" type="submit">
-            Submit
-          </button>
-        </Form>
+        {({ isSubmitting }) => (
+          <Form>
+            <InputText label="Invoice #" name="invNbr" type="number" />
+            <InputSelect label="Client" name="clientId" options={clients} />
+            <InputText label="Total Amount" name="totalAmount" type="number" />
+            <InputText label="Invoice Date" name="date" type="number" />
+            <button disabled={isSubmitting} className="btn btn-primary mt-5" type="submit">
+              Submit
+            </button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
