c9c79313001fa24ca49250144af502ba2083a056
zc0rp10 <zcorpio@gmail.com>
2020-04-10 13:38:49 +0200

    InputSelect  now renders options dynamicly.

--- src/components/common/InputSelect.js
+++ src/components/common/InputSelect.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { useField } from "formik";
 
-const InputSelect = ({ label, ...props }) => {
+const InputSelect = ({ label, options, ...props }) => {
   const [field, meta] = useField(props);
+
+  const renderOptions = options => {
+    const allOptions = options.map(client => (
+      <option key={client._id} value={client._id}>
+        {client.name}
+      </option>
+    ));
+    return allOptions;
+  };
+
   return (
     <div className="mb-4">
       <div className="inline-block relative w-64">
@@ -17,7 +27,10 @@ const InputSelect = ({ label, ...props }) => {
             className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
             {...field}
             {...props}
-          />
+          >
+            <option value="">Select a client</option>
+            {renderOptions(options)}
+          </select>
           <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
             <svg
               className="fill-current h-4 w-4"
